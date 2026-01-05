766a572c45468e5e46db28f9d095024021877660
𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 <botwhatsapp248@gmail.com>
2026-01-05 01:52:04 +0100

    Update malvin.js

--- malvin.js
+++ malvin.js
@@ -3,20 +3,24 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
+let server = require('./qr'),
+    code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/qr', server);
 app.use('/code', code);
-app.use('/',async (req, res, next) => {
+app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
-});
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/main.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Deployment Successful!
+Don't Forget To Give Star
 
- Session-Server Running on http://localhost:` + PORT)
+ Server running on http://localhost:` + PORT)
 })
 
 module.exports = app
-       
